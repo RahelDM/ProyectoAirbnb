522a375fb90e0f2995792f7f9e295f6f28c0fda3
Raquel S. Álvarez <raquelsa34@educastur.es>
2024-01-26 13:40:25 +0100

    Comienzo de la lectura de datos.

--- src/main/java/naranco/dam/proyectoalojamientos/AppStartupRunner.java
+++ src/main/java/naranco/dam/proyectoalojamientos/AppStartupRunner.java
@@ -37,15 +37,51 @@ public class AppStartupRunner implements ApplicationListener<ApplicationReadyEve
     }
 
     private void cargarDatosDeFichero() {
-
+        int i=0;
         try (BufferedReader br = new BufferedReader(
                 new FileReader(environment.getProperty("app.data.filename")))) {
             String linea = br.readLine();
             while ((linea = br.readLine()) != null) {
                 String[] campos = linea.split(",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)");
 
-                //Lectura del fichero
-                System.out.println("ndjsf");
+                Long idAlojamiento = Long.parseLong(campos[0]);
+                String datosAlojamiento = campos[1];
+                Long idPropietario = Long.parseLong(campos[2]);
+                String nombrePropietario = campos[3];
+                String nombreBarrio = campos[4];
+                String nombreDistrito = campos[5];
+                double latitud = Double.parseDouble(campos[6]);
+                double longitud = Double.parseDouble(campos[7]);
+                String nombreHabitacion = campos[8];
+                double precio =  Double.parseDouble(campos[9]);
+
+                double puntuacion;
+                try {
+                     puntuacion =  Double.parseDouble(campos[10]);
+                }catch (NumberFormatException e){
+                    puntuacion=0;
+                }
+
+                int habitaciones;
+                try {
+                    habitaciones = Integer.parseInt(campos[11]);
+
+                }catch (NumberFormatException e){
+
+                }
+
+                int camas = Integer.parseInt(campos[12]);
+
+                int banos;
+                try {
+                     banos = Integer.parseInt(campos[13]);
+                }catch (NumberFormatException e){
+                    banos=0;
+                }
+
+
+                System.out.println(i++);
+
             }
         } catch (IOException e) {
             throw new RuntimeException(e);
